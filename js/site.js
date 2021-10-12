d0010333f9d506f22e268ec3b8422d8f6fa3e30b
shiori102 <shiori10296@yahoo.com>
2021-10-11 22:21:08 -0400

    code changes

--- js/site.js
+++ js/site.js
@@ -7,25 +7,38 @@ document.getElementById("calculateBtn").onclick = function () {
 
     //parseInt the loan amount bc it would read NaN(not a number)
     amount = parseInt(amount);
-    document.getElementById("principalAmount").innerHTML = `$${amount}`;
+    let displayAmount = amount.toLocaleString(undefined, {
+        style: "currency",
+        currency: "USD"
+    })
+    document.getElementById("principalAmount").innerHTML = `${displayAmount}`;
 
     //created the first formula of total monthly payment (use math.pow to create exponent)
     //toFixed(2) displays the number to 2 decimal places
     let rowData = "";
     let totalMonthlyPayment = (amount) * (rate / 1200) / (1 - (Math.pow(1 + rate / 1200, -months)));
-    totalMonthlyPayment = totalMonthlyPayment;
-    document.getElementById("getMonthlyPayment").innerHTML = `$${totalMonthlyPayment}`;
+    let displayPayment = totalMonthlyPayment.toLocaleString(undefined, {
+        style: "currency",
+        currency: "USD"
+    })
+    document.getElementById("getMonthlyPayment").innerHTML = `${displayPayment}`;
     let totalLoanAmount = months * totalMonthlyPayment;
 
-    totalLoanAmount = totalLoanAmount;
+    let displayTotal = totalLoanAmount.toLocaleString(undefined, {
+        style: "currency",
+        currency: "USD"
+    })
 
     //adding the dollar sign to the numbers
-    document.getElementById("totalCost").innerHTML = `$${totalLoanAmount}`;
+    document.getElementById("totalCost").innerHTML = `${displayTotal}`;
 
 
     let interest = totalLoanAmount - amount;
-    interest = interest;
-    document.getElementById("totalInterest").innerHTML = `$${interest}`;
+    let displayInterest = interest.toLocaleString(undefined, {
+        style: "currency",
+        currency: "USD"
+    })
+    document.getElementById("totalInterest").innerHTML = `${displayInterest}`;
 
     let interestPayment = totalLoanAmount * rate / 1200;
     let principalPayment = totalMonthlyPayment - interestPayment;
@@ -41,6 +54,7 @@ document.getElementById("calculateBtn").onclick = function () {
     let currentBalance = amount;
     let interestSum = 0;
 
+
     //for loop to reiterate over the number of months entered in the form
     //i=1 bc thats when the table should start
     //calculations and formulas are created to make sure that the numbers are changing after every month
@@ -54,11 +68,11 @@ document.getElementById("calculateBtn").onclick = function () {
 
         //adding the table rows to the table by using the dataRow.getElementById and referring back the the ids named in the html
         dataRow.getElementById("month").textContent = i;
-        dataRow.getElementById("payment").textContent = totalMonthlyPayment;
+        dataRow.getElementById("payment").textContent = totalMonthlyPayment.toFixed(2);
         dataRow.getElementById("principal").textContent = principalPayment.toFixed(2);
         dataRow.getElementById("interest").textContent = interestPayment.toFixed(2);
         dataRow.getElementById("totalInterest").textContent = interestSum.toFixed(2);
-        dataRow.getElementById("balance").textContent = remainingBalance.toFixed(2);
+        dataRow.getElementById("balance").textContent = Math.abs(remainingBalance.toFixed(2));
 
         resultsBody.appendChild(dataRow);
 
@@ -66,9 +80,3 @@ document.getElementById("calculateBtn").onclick = function () {
 
 }
 
-// HOW TO ADD COMMAS
-// number.toLocaleString(undefined,
-// {
-//     'minimumFractionDigits': 2,
-//     'maximumFractionDigits': 2
-// });
